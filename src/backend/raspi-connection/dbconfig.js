b7067180ea894e4641353120bd66c04fcfd1ba4f
yeonod <bluelayzxx@gmail.com>
2023-12-04 09:44:05 +0900

    backend/raspi-connection 디렉터리 및 파일 추가
    센서와 수신기(라즈) 간 통신을 위한 작업 폴더 추가

---
+++ src/backend/raspi-connection/dbconfig.js
@@ -0,0 +1,8 @@
+const dbConfig = {
+  host: "localhost",
+  user: "your_mysql_username",
+  password: "your_mysql_password",
+  database: "your_database",
+};
+
+module.exports = dbConfig;
---
+++ src/backend/raspi-connection/receive.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+const dbConfig = require("./dbconfig");
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+// MySQL 연결 생성
+const connection = mysql.createConnection(dbConfig);
+
+// 연결 테스트
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to MySQL:", err);
+  } else {
+    console.log("Connected to MySQL");
+  }
+});
+
+// POST 요청 처리
+app.post("/receive_data", (req, res) => {
+  try {
+    const data = req.body; // 받은 JSON 데이터를 파싱합니다.
+    console.log("Received Data:", data);
+
+    // 필요한 데이터 추출
+    const { sesor_id, temp, humi, gas } = data;
+
+    // MySQL에 데이터 삽입
+    const query = `INSERT INTO sensor_data (sesor_id, temp, humi, gas) VALUES (?, ?, ?, ?)`;
+    connection.query(query, [sesor_id, temp, humi, gas], (err, results) => {
+      if (err) {
+        console.error("Error inserting data into MySQL:", err);
+        throw err;
+      }
+
+      // 여기에 필요한 동작을 추가할 수 있습니다.
+
+      console.log("Data inserted into MySQL:", results);
+
+      const response = {
+        status: "success",
+        message: "Data received and stored successfully",
+      };
+      res.json(response);
+    });
+  } catch (error) {
+    console.error("Error:", error);
+    const response = {
+      status: "error",
+      message: "Failed to process and store the data",
+    };
+    res.status(500).json(response); // 500은 서버 내부 오류를 의미합니다.
+  }
+});
+
+// 서버 종료 시 MySQL 연결 종료
+process.on("SIGINT", () => {
+  connection.end();
+  process.exit();
+});
+
+// 서버 시작
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
