7d079b47bf443f00dfe79ee130fd8a76b17f342c
GyuPin.Moon <gyupin.moon@gmail.com>
2023-12-06 10:49:22 +0900

    Update fire_detection.py
    Update fire_detection.py

--- src/backend/fire_detection.py
+++ src/backend/fire_detection.py
@@ -2,65 +2,70 @@ import json
 import requests
 import time
 
-# 화재 감지 조건을 결정하는 함수 정의
-def determine_fire_conditions(hum, base_temp=60, base_gas=300):
-    if hum <= 29:  # 낮은 습도
-        return base_temp * 0.8, base_gas * 0.8
-    elif hum >= 61:  # 높은 습도
-        return base_temp * 1.2, base_gas * 1.2
-    else:  # 보통 습도
-        return base_temp, base_gas
+def calculate_averages_and_detect_fire(sensor_data, recent_temps):
+    sum_data = {'temperature': 0, 'humidity': 0, 'gas': 0}
 
-# 데이터 업데이트 함수 정의
-def update_fire_status(data, recent_temps):
-    for entry in data:
-        # 습도에 따른 화재 감지 조건 설정
-        fire_temp, fire_gas = determine_fire_conditions(entry['hum'])
+    for entry in sensor_data:
+        sum_data['temperature'] += entry['temperature']
+        sum_data['humidity'] += entry['humidity']
+        sum_data['gas'] += entry['gas']
 
-        # 온도 변화량 체크
-        avg_temp = sum(recent_temps) / len(recent_temps) if recent_temps else 0
-        temp_change_fire = entry['temp'] >= 2 * avg_temp
+    avg_data = {key: value / len(sensor_data) for key, value in sum_data.items()}
 
-        # 화재 조건 체크
-        if entry['temp'] >= fire_temp or entry['gas'] >= fire_gas or temp_change_fire:
-            entry['fire'] = 1
+    def determine_fire_conditions(hum, base_temp=60, base_gas=3000):
+        if hum <= 29:
+            return base_temp * 0.8, base_gas * 0.8
+        elif hum >= 61:
+            return base_temp * 1.2, base_gas * 1.2
         else:
-            entry['fire'] = 0
+            return base_temp, base_gas
 
-        # 최근 온도 리스트 업데이트
-        recent_temps.append(entry['temp'])
-        if len(recent_temps) > 3:
-            recent_temps.pop(0)
+    fire_temp, fire_gas = determine_fire_conditions(avg_data['humidity'])
 
-    return data
+    # 이전 온도 평균 계산
+    avg_temp = (
+        sum(recent_temps) / len(recent_temps)
+    ) if recent_temps else 0
+
+    # 온도 변화량이 2배 이상일 경우 화재 판단
+    temp_change_fire = False
+    if len(recent_temps) >= 3:
+        avg_temp = sum(recent_temps) / len(recent_temps)
+        temp_change_fire = avg_data['temperature'] >= 2 * avg_temp
+
+    fire_status = 1 if (
+        avg_data['temperature'] >= fire_temp 
+        or avg_data['gas'] >= fire_gas 
+        or temp_change_fire
+    ) else 0
+
+    # 최근 온도 리스트 업데이트
+    recent_temps.append(avg_data['temperature'])
+    if len(recent_temps) > 3:
+        recent_temps.pop(0)
+
+    return avg_data, fire_status
 
 # 파일 경로
-file_path = 'temp.json'  # 실제 파일 경로
+file_path = 'sensor_data.json'  # 실제 파일 경로
 
 # 최근 온도 값을 저장하는 리스트
 recent_temperatures = []
 
 # 무한 루프
 while True:
-    # 파일을 읽음
     with open(file_path, 'r') as file:
-        data = json.load(file)
+        sensor_data = json.load(file)
 
-    # 데이터 업데이트
-    updated_data = update_fire_status(data, recent_temperatures)
+    avg_data, fire_status = calculate_averages_and_detect_fire(sensor_data, recent_temperatures)
+    avg_data['fire'] = fire_status
 
-    # 업데이트된 데이터를 JSON 형식으로 변환
-    json_data = json.dumps(updated_data)
+    print(avg_data)
 
-    # POST 요청을 보낼 URL
-    url = "http://192.168.4.1"
+    url = "http://ceprj.gachon.ac.kr:60035"
+    response = requests.post(url, data=json.dumps(avg_data), auth=('t23320', 'dovev2023'))
 
-    # POST 요청 보내기
-    response = requests.post(url, data=json_data)
-
-    # 응답 상태 코드와 내용 출력
     print("Status Code:", response.status_code)
     print("Response:", response.text)
 
-    # 1초 대기
     time.sleep(1)
