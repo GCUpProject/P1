44815d9a5d7748f6bf244cb75c79a5ffe9e92766
YoonSoo Kim <rabbit7183@gmail.com>
2023-12-11 21:27:25 +0900

    Add: TTS logic and gas sensor(MQ-5) data collect function
    Add: TTS logic and gas sensor(MQ-5) data collect function
    
     TTS logic and gas sensor(MQ-5) data collect function at connection.py (raspberrypi main file)

--- src/backend/raspi-connection/local-raspi/connection.py
+++ src/backend/raspi-connection/local-raspi/connection.py
@@ -1,16 +1,15 @@
 import requests
 import Adafruit_DHT
 import time
-import random
 import json
+from gtts import gTTS
 from multiprocessing import Process
 import os
+import spidev
 #----------------DHT-----------------
 sensor = Adafruit_DHT.DHT11
 pin = 23
-#----------------MQ5-----------------
-
-#-------------SendingData------------
+#-------------SENDINGDATA------------
 remote_host = '192.168.4.1'
 remote_user = 'pi'
 remote_path = '/home/pi/Server/'
@@ -20,24 +19,62 @@ sensorId = 1
 #----------Create a JSON file--------
 i = 0
 dataList = []
+#----------WARNINGSET---------------
+warn_fileName = "warning.json"
 
-   
+#------------------------------------MQ5-----------------------------------------
+class McpEx:
+    def __init__(self):
+        self.spi = spidev.SpiDev()
+        self.spi.open(0, 0) 
+        self.spi.mode = 3
+        self.spi.max_speed_hz = 1000000
+ 
+    def analog_read(self, channel):
+        r = self.spi.xfer2([1, (0x08 + channel) << 4, 0])
+        adc_out = ((r[1] & 0x03) << 8) + r[2]
+        return adc_out
+ 
+    def __call__(self):
+        while True:
+            adc = self.analog_read(0)
+            voltage = adc * 5 / 1023
+            #print("ADC = %s(%d) Voltage = %.3fV" % (hex(adc), adc, voltage))
+            return adc         
+gas = McpEx()
+
+#----------------------------WARNINGPROCESS-----------------------------------
+def warn_process(n):
+	current_directory = os.path.dirname(os.path.realpath(__file__))
+	file_path = os.path.join(current_directory, warn_fileName)
+	while True:
+		if os.path.exists(file_path):
+			with open("warning.json", "r") as json_file:
+				data = json.load(json_file)
+			text_value = data.get('text', '')
+			tts = gTTS(text=text_value, lang='ko', slow=False)
+			tts.save("output.mp3")
+			os.system("xdg-open output.mp3")
+		time.sleep(10)
+
+#---------------------------------MAIN-----------------------------------------
 if __name__ == '__main__':
 	
-	while True:
-		
+	#Warning process on
+	warn_p = Process(target=warn_process, args=(1,))
+	warn_p.start()
+	
+	while True:	
 		#Check sensor data
 		humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+		gasAdc = gas.__call__()
 		
 		# Setting data for Server
 		if humidity is not None and temperature is not None:
 			print(f'index={i}')
-			print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
-			# Mq-5, fix
-			gas = round(random.uniform(1600, 1620),4)
-			
-			data = {'sensor_id': sensorId, 'temperature': temperature, 'humidity': humidity, 'gas': gas}
+			data = {'sensor_id': sensorId, 'temperature': temperature, 'humidity': humidity, 'gas': gasAdc}
 			dataList.append(data)
+			print(data)
 			i += 1
 		
 		# Post to server
@@ -53,3 +90,4 @@ if __name__ == '__main__':
 			result = os.system(scp_command)
 		
 		time.sleep(0.04)  # 25Hz
+
